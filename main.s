a9e99d7c35d39171c7c0a3eac549abfb28133cc4
wl2923 <wl2923@skbl-418-014.ph.ic.ac.uk>
2025-10-31 12:43:34 +0000

    minor changes from port B to C

--- main.s
+++ main.s
@@ -8,17 +8,20 @@ main:
 
 	org	0x100		    ; Main code starts here at address 0x100
 start:
-	movlw 	0x0
-	movwf	TRISC, A	    ; Port C all outputs
-	bra 	test
+  	movlw 	0x0
+	movwf	TRISC, A            ; Port C all outputs
+	movlw   0x0F
+	movwf   0x90, A
+	movlw   0x0
+	bra 	loop                           ;movff 	0x06, PORTC
 loop:
-	movff 	0x06, PORTC
-	incf 	0x06, W, A
-test:
-	movwf	0x06, A	    ; Test for end of loop condition
-	movlw 	0x63
-	cpfsgt 	0x06, A
-	bra 	loop		    ; Not yet finished goto start of loop again
-	goto 	0x0		    ; Re-run program from start
-
+	movwf   FSR0L
+	clrf    FSR0H
+	movwf   INDF0
+	addlw   0x01
+	movf    WREG, W 
+	cpfslt  0x90, A
+	bra     loop
+	nop
+        
 	end	main
